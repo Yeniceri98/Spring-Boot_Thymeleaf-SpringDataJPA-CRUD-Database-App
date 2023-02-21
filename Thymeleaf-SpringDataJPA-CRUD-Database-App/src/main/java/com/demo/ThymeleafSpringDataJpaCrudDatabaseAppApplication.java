3b5962cd1370828fbe0f10314406dfd3d784fed0
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2023-02-21 13:58:46 +0300

    employees fetched from db

--- Thymeleaf-SpringDataJPA-CRUD-Database-App/src/main/java/com/demo/ThymeleafSpringDataJpaCrudDatabaseAppApplication.java
+++ Thymeleaf-SpringDataJPA-CRUD-Database-App/src/main/java/com/demo/ThymeleafSpringDataJpaCrudDatabaseAppApplication.java
@@ -1,9 +1,11 @@
 package com.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration		// NOT: @SpringBootApplication anotasyonu bunu da kapsar yani gereksizdir fakat eklemeyince Controller classında injection yaparken hata veriyor
 public class ThymeleafSpringDataJpaCrudDatabaseAppApplication {
 
 	public static void main(String[] args) {
--- Thymeleaf-SpringDataJPA-CRUD-Database-App/src/main/java/com/demo/service/EmployeeServiceImpl.java
+++ Thymeleaf-SpringDataJPA-CRUD-Database-App/src/main/java/com/demo/service/EmployeeServiceImpl.java
@@ -3,10 +3,12 @@ package com.demo.service;
 import com.demo.dao.EmployeeRepository;
 import com.demo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
